aecee30876ab01aca0b2c47c1e03e664243ad45d
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2022-05-04 16:29:41 +0300

    completing CurrenciesBeanTests part

--- src/main/java/com/epam/rd/jsp/currencies/Currencies.java
+++ src/main/java/com/epam/rd/jsp/currencies/Currencies.java
@@ -1,26 +1,29 @@
 package com.epam.rd.jsp.currencies;
 
 import java.math.BigDecimal;
-import java.util.Collection;
-import java.util.Map;
-import java.util.TreeMap;
+import java.math.RoundingMode;
+import java.util.*;
 
 public class Currencies {
-    private Map<String, BigDecimal> curs = new TreeMap<>();
-
+    private final Map<String, BigDecimal> currenciesData = new TreeMap<>();
     public void addCurrency(String currency, BigDecimal weight) {
-        throw new UnsupportedOperationException();
+        currenciesData.put(currency, weight);
     }
 
     public Collection<String> getCurrencies() {
-        throw new UnsupportedOperationException();
+        return currenciesData.keySet();
     }
 
     public Map<String, BigDecimal> getExchangeRates(String referenceCurrency) {
-        throw new UnsupportedOperationException();
+        Map<String, BigDecimal> newMap = new TreeMap<>();
+        if (currenciesData.containsKey(referenceCurrency)) {
+            currenciesData.forEach((String key, BigDecimal element) -> newMap.put(key,
+                    currenciesData.get(referenceCurrency).divide(element,5,RoundingMode.HALF_UP)));
+        }
+        return newMap;
+    }
+    public BigDecimal convert(BigDecimal amount, String sourceCurrency, String targetCurrency) {
+        return currenciesData.get(sourceCurrency).multiply(amount).divide(currenciesData.get(targetCurrency),5,RoundingMode.HALF_UP);
     }
 
-    public BigDecimal convert(BigDecimal amount, String sourceCurrency, String targetCurrency) {
-        throw new UnsupportedOperationException();
-    }
 }
